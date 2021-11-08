795641f10f2f84b5f269dc478f779692f964e225
Tal Barzilay <talbarzi@post.bgu.ac.il>
2021-11-08 21:08:00 +0200

    added user controller tests

--- src/test/java/com/project/reddital_backend/controllers/api/UserControllerTest.java
+++ src/test/java/com/project/reddital_backend/controllers/api/UserControllerTest.java
@@ -1,15 +1,17 @@
 package com.project.reddital_backend.controllers.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
 import com.project.reddital_backend.DTOs.models.UserDto;
 import com.project.reddital_backend.controllers.requests.Request;
 import com.project.reddital_backend.controllers.requests.UserSignupRequest;
+import com.project.reddital_backend.exceptions.DuplicateEntityException;
 import com.project.reddital_backend.services.UserService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,11 +22,9 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import static org.mockito.ArgumentMatchers.any;
 
@@ -41,12 +41,12 @@ public class UserControllerTest {
     private MockMvc mockMvc;
 
 
-
-    private UserDto user;
-
-    private final String USER_PATH = "/user";
-
     private ObjectMapper objectMapper;
+
+    private final String USER_PATH  = "/user";
+    private final String username   = "Sportalcraft";
+    private final String email      = "test@gmail.com";
+    private final String password   = "12345678";
 
 
     // ------------------------------------------------------- preparations -------------------------------------------------------
@@ -55,20 +55,13 @@ public class UserControllerTest {
     public void setUp() {
         openMocks(this);
 
-        user = UserDto.builder()
-                .username("Sportalcraft")
-                .email("test@gmail.com")
-                .password("123456")
-                .build();
-
         objectMapper = new ObjectMapper();
     }
 
     @AfterEach
     public void tearDown() {
         mockUserService = null;
-        user = null;
-        //objectMapper = null;
+        objectMapper = null;
     }
 
     // ----------------------------------------------------- tests -----------------------------------------------------
@@ -77,20 +70,99 @@ public class UserControllerTest {
     @Test
     @DisplayName("test signup with good parameters")
     public void signup_good() throws Exception {
-        Mockito.when(mockUserService.signup(any()))
-                .thenReturn(null);
+        Mockito.when(mockUserService.signup(any())).thenAnswer((Answer<UserDto>) invocation -> {
+            Object[] args = invocation.getArguments();
+            return (UserDto) args[0];
+        });
 
         Request request = UserSignupRequest.builder()
-                        .username(user.getUsername())
-                        .email(user.getEmail())
-                        .password(user.getPassword()).build();
+                        .username(username)
+                        .email(email)
+                        .password(password)
+                .build();
 
+        ResultActions result = post(USER_PATH + "/signup", requestAsString(request));
+        String body = result.andReturn().getResponse().getContentAsString();
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post(USER_PATH + "/signup")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request))
-        );
+        result.andExpect(status().isOk());
 
-        result.andDo(print());
+        checkProperty(username, "$.payload.username", body);
+        checkProperty(email, "$.payload.email", body);
+
+        try {
+            checkProperty("", "$.payload.password", body);
+        } catch (PathNotFoundException ignored) {}
+
     }
+
+    @Test
+    @DisplayName("test signup with an existing username")
+    public void signup_duplicate() throws Exception {
+        Mockito.when(mockUserService.signup(any()))
+                .thenThrow(new DuplicateEntityException("EXIST"));
+
+        Request request = UserSignupRequest.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .build();
+
+        ResultActions result = post(USER_PATH + "/signup", requestAsString(request));
+        String body = result.andReturn().getResponse().getContentAsString();
+
+        checkProperty("DUPLICATE_ENTITY", "$.status", body);
+        checkProperty("EXIST", "$.msg", body);
+
+        checkNonProperty( "$.payload.username", body);
+    }
+
+    @Test
+    @DisplayName("test signup with missing username")
+    public void signup_missingUsername() throws Exception {
+        Mockito.when(mockUserService.signup(any())).thenAnswer((Answer<UserDto>) invocation -> {
+            Object[] args = invocation.getArguments();
+            return (UserDto) args[0];
+        });
+
+        ResultActions result = post(USER_PATH + "/signup", String.format("{\"email\": \"%s\", \"password\":\"%s\"}", email, password));
+        String body = result.andReturn().getResponse().getContentAsString();
+
+        checkProperty("BAD_REQUEST", "$.status", body);
+
+        checkNonProperty( "$.payload.username", body);
+    }
+
+
+
+
+
+    // ----------------------------------------------------- private methods -----------------------------------------------------
+
+    private void checkProperty(String expected, String jsonPath, String body){
+        assertEquals(expected, JsonPath.read(body, jsonPath));
+    }
+
+    private void checkNonProperty(String jsonPath, String body){
+        try {
+            JsonPath.read(body, jsonPath);
+        } catch (PathNotFoundException ignored) {
+            return;
+        }
+
+        Assertions.fail("the property " + jsonPath + " exist in " + body + ", but it shouldn't");
+    }
+
+
+
+    private ResultActions post(String uri, String content) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+        );
+    }
+
+    private String requestAsString(Request request) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(request);
+    }
+
 }
