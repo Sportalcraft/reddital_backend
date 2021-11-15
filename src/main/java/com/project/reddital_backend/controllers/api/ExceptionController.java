f927965f050f53ecbdcf9db48388fa005bf83af9
Tal Barzilay <talbarzi@post.bgu.ac.il>
2021-11-15 13:22:55 +0200

    added a controller for erros

---
+++ src/main/java/com/project/reddital_backend/controllers/api/ExceptionController.java
@@ -0,0 +1,48 @@
+package com.project.reddital_backend.controllers.api;
+
+import com.project.reddital_backend.exceptions.BadParametersException;
+import com.project.reddital_backend.exceptions.DuplicateEntityException;
+import com.project.reddital_backend.exceptions.EntityNotFoundException;
+import com.project.reddital_backend.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/")
+public class ExceptionController {
+
+    //The goal of this controller is to handle exceptions
+
+    // -------------------------------------- exception handlers --------------------------------------
+
+    @ExceptionHandler(DuplicateEntityException.class)
+    public final ResponseEntity<String> handle(DuplicateEntityException ex) {
+        return ResponseEntity.unprocessableEntity()
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadParametersException.class)
+    public final ResponseEntity<String> handle(BadParametersException ex) {
+        return ResponseEntity.badRequest()
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public final ResponseEntity<String> handle(EntityNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public final ResponseEntity<String> handle(UnauthorizedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(ex.getMessage());
+    }
+}
