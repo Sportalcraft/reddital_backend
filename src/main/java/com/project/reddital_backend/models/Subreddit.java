9dca76635eef21aca008207fdf9538f204fc0986
Tal Barzilay <talbarzi@post.bgu.ac.il>
2021-11-15 12:27:48 +0200

    adeed subreddit

---
+++ src/main/java/com/project/reddital_backend/models/Subreddit.java
@@ -0,0 +1,28 @@
+package com.project.reddital_backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@Table(name = "subreddits")
+public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id; //PK
+
+
+    @Column(name = "name")
+    private String name;
+}
